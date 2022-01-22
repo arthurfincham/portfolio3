810aadca78843d18acdb715fc838f5b11f270085
Arthur Fincham <arthurfincham@me.com>
2022-01-22 23:06:37 +0000

    add navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,15 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Navbar() {
-  return <div className="w-full bg-slate-300 h-[60px]"></div>;
+  return (
+    <nav className="w-full bg-slate-300 h-[60px] fr justify-end px-8">
+      <div className="fr space-x-6 items-center">
+        <NavLink to="/">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/Education">Education</NavLink>
+        <NavLink to="/Experience">Experience</NavLink>
+        <NavLink to="/Contact">Contact</NavLink>
+      </div>
+    </nav>
+  );
 }
