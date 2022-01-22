2324d84a43eabcf249b91a2553fea813f9dfa0e6
Arthur Fincham <arthurfincham@me.com>
2022-01-22 22:29:30 +0000

    remove unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-2xl text-white bg-red-400">hello</h1>
+    </div>
+  );
 }
 
 export default App;
