77e53b5cb59b9b24c468fa031f8277f1c9d4bae5
Arthur Fincham <arthurfincham@me.com>
2022-01-24 12:42:06 +0000

    add pin icon to projects section

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,17 +2,18 @@ import { iconSelector } from '../svgs/devIcons/iconSelector';
 import { useSpring, animated } from 'react-spring';
 import useMeasure from 'react-use-measure';
 import { useState } from 'react';
+import PinIcon from '../svgs/PinIcon';
+import PlusIcon from '../svgs/PlusIcon';
 
 export default function ProjectItem({ project, className }) {
   const [isClosed, setIsClosed] = useState(true);
+
+  const [isPinned, setIsPinned] = useState(false);
 
   const [ref, bounds] = useMeasure();
 
   const [imageRef, imageBounds] = useMeasure();
 
-  const infoStyle = useSpring({
-    height: isClosed ? 0 : bounds.height,
-  });
   const capitalize = (str) => {
     return str.toUpperCase();
   };
@@ -23,6 +24,30 @@ export default function ProjectItem({ project, className }) {
 
   const handleExit = () => {
     setIsClosed(!isClosed);
+  };
+
+  const handleClick = () => {
+    setIsPinned(!isPinned);
+  };
+
+  const infoMaster = () => {
+    if (isPinned) {
+      return false;
+    } else if (!isClosed) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const infoStyle = useSpring({
+    height: infoMaster() ? 0 : bounds.height,
+  });
+
+  const pinStyle = {
+    fill: isPinned ? 'black' : 'none',
+    stroke: isPinned ? '#FFF4C7' : 'black',
+    strokeWidth: isPinned ? '5px' : '25px',
   };
 
   const Link = ({ url }) => {
@@ -36,22 +61,28 @@ export default function ProjectItem({ project, className }) {
   };
 
   const Tab = ({ children }) => {
-    return <div className="h-auto p-1 px-2 border-2 border-b-0 shadow-lg rounded-t-xl bg-amber-100">{children}</div>;
+    return (
+      <div
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleExit}
+        onClick={handleClick}
+        className="flex items-center h-auto p-1 px-2 space-x-1 border-2 border-b-0 rounded-t-lg shadow-lg cursor-pointer bg-amber-100 hover:bg-amber-300/40"
+      >
+        {children}
+      </div>
+    );
   };
 
   const LinkTab = ({ children }) => {
-    return <div className="h-auto p-1 px-2 border-2 border-b-0 shadow-lg rounded-t-xl bg-amber-100 hover:border-indigo-500">{children}</div>;
-  };
-
-  const subTitleStyle = {
-    height: imageBounds.height,
+    return <div className="h-auto p-1 px-2 border-2 border-b-0 rounded-t-lg shadow-lg bg-amber-100 hover:bg-amber-300/40">{children}</div>;
   };
 
   return (
     <div className="border-2 rounded-lg fcc">
       <div className="items-end w-full px-4 pt-2 space-x-2 border-b-2 rounded-t-lg shadow-lg bg-amber-50 fr">
         <Tab>
-          <span className="font-mono1">{project.title}</span>
+          {isClosed ? <PlusIcon className="w-[20px] h-[20px] scale-[1.3]" /> : <PinIcon styles={pinStyle} className="w-[20px] h-[20px]" />}
+          <span className="text-xl font-mono1">{project.title}</span>
         </Tab>
         <LinkTab>
           <a href={project.repoURL} target="blank" className="text-sm font-mono3">
@@ -60,7 +91,7 @@ export default function ProjectItem({ project, className }) {
         </LinkTab>
         {project.liveLink ? <Link url={project.liveLink} /> : ''}
       </div>
-      <div className={className} onMouseEnter={handleEnter} onMouseLeave={handleExit} ref={imageRef}>
+      <div className={className} ref={imageRef}>
         <img src={project.imagePath} className="w-full rounded-b-lg shadow-lg projectImage" />
         <animated.div style={infoStyle} className="absolute bottom-0 w-full overflow-hidden rounded-lg shadow-lg bg-amber-100">
           <div ref={ref} className="w-full px-2 pb-4">
---
+++ src/svgs/PinIcon.js
@@ -0,0 +1,12 @@
+export default function PinIcon({ className, styles }) {
+  return (
+    <svg style={styles} className={className} width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 557.93 715.02">
+      <g>
+        <path d="M655,410.76,527.36,376.61,304.47,490.17,257.09,613.49Z" transform="translate(-97.03 -18.49)" />
+        <path d="M300.79,469,512.39,361.2,392.62,126.14,181,234Z" transform="translate(-97.03 -18.49)" />
+        <path d="M162.53,220.35l0,0L422.66,87.8,387.34,18.49,97,166.42l35.32,69.31Z" transform="translate(-97.03 -18.49)" />
+        <path d="M418.54,554.27,569.72,733.51l-57.53-227Z" transform="translate(-97.03 -18.49)" />
+      </g>
+    </svg>
+  );
+}
