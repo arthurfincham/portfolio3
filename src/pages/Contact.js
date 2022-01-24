d4895cd74539cbe88ab638975a71c02102080577
Arthur Fincham <arthurfincham@me.com>
2022-01-24 13:47:57 +0000

    add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -31,59 +31,59 @@ export default function Contact() {
     e.target.reset();
   };
   return (
-    <>
-      <section className="relative bg-white dark:bg-gray-800 dark:text-white text-black body-font h-[90vh] lg:h-[80vh]  flex items-center">
-        <div className="container px-5 py-12 mx-auto md:w-3/4">
-          <div className="flex flex-col items-center justify-center w-full mb-2 text-center">
-            <p className="py-6 text-2xl font-bold leading-relaxed tracking-wide handwriting lg:w-2/3">Get in touch.</p>
+    <div className="container px-5 py-12 mx-auto max-w-[600px]">
+      <div className="w-full mb-2 text-center fcc">
+        <p className="py-6 text-2xl font-bold leading-relaxed tracking-wide font-mono3 lg:w-2/3">GET IN TOUCH</p>
+      </div>
+      <div className="w-full">
+        <form ref={form} onSubmit={sendEmail} className="w-full fcc">
+          <div className="w-full fcc xs:flex-row">
+            <div className="w-full xs:w-1/2 xs:mr-2">
+              <div className="relative">
+                <label htmlFor="name" className="text-sm leading-7 font-mono3 ">
+                  NAME
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="user_name"
+                  className="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 font-a4 focus:ring-indigo-200"
+                />
+              </div>
+            </div>
+            <div className="w-full xs:w-1/2 xs:ml-2">
+              <div className="relative">
+                <label htmlFor="email" className="text-sm leading-7 font-mono3 ">
+                  EMAIL
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="user_email"
+                  className="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 font-a4 focus:ring-indigo-200"
+                />
+              </div>
+            </div>
           </div>
-          <div className="mx-auto lg:w-1/2 md:w-2/3">
-            <form ref={form} onSubmit={sendEmail} className="flex flex-wrap -m-2">
-              <div className="w-1/2 p-2">
-                <div className="relative">
-                  <label htmlFor="name" className="text-sm leading-7 ">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="user_name"
-                    className="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                  />
-                </div>
-              </div>
-              <div className="w-1/2 p-2">
-                <div className="relative">
-                  <label htmlFor="email" className="text-sm leading-7 ">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="user_email"
-                    className="w-full px-3 py-1 text-base leading-8 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                  />
-                </div>
-              </div>
-              <div className="w-full p-2">
-                <div className="relative">
-                  <label htmlFor="message" className="text-sm leading-7 ">
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    className="w-full h-32 px-3 py-1 text-base leading-6 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="w-full p-2">
-                <button className="flex px-8 py-2 mx-auto text-lg border-0 rounded focus:outline-none bg-blue-gradient">Send</button>
-              </div>
-            </form>
+          <div className="w-full">
+            <div className="relative">
+              <label htmlFor="message" className="text-sm leading-7 font-mono3 ">
+                MESSAGE
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full h-32 px-3 py-1 text-base leading-6 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:bg-white focus:ring-2 font-a4 focus:ring-indigo-200"
+              ></textarea>
+            </div>
           </div>
-        </div>
-      </section>
-    </>
+          <div className="w-full p-2">
+            <button className="flex px-4 py-2 mx-auto text-lg border-[.1em] rounded-lg hover:bg-amber-200/50 border-black rounded font-a2 focus:outline-none bg-blue-gradient">
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        xs: '440px',
         sm: '640px',
         md: '768px',
         lg: '1024px',
