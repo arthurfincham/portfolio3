5f268fbd3f4ba252888dc97e8fcea7d363990c78
Arthur Fincham <arthurfincham@me.com>
2022-01-30 17:52:13 +0000

    add import sorting rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import * as Pages from './pages/pagesExporter';
-import Navbar from './components/Navbar';
-import useGoogleAnalytics from './utils/useGoogleAnalytics';
 import { useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import React from 'react';
+
+import Navbar from './components/Navbar';
+import * as Pages from './pages/pagesExporter';
+import useGoogleAnalytics from './utils/useGoogleAnalytics';
+
 
 function GARoutes() {
   useGoogleAnalytics();
@@ -16,7 +19,7 @@ function GARoutes() {
       <main className="dark:bg-slate-900">
         <TransitionGroup component={null}>
           <CSSTransition key={location.key} classNames="fade" timeout={300}>
-            <Routes location={location}>
+            <Routes location={location}>    
               <Route path="/" element={<Pages.About />} />
               <Route path="/projects" element={<Pages.Projects />} />
               <Route path="/CV" element={<Pages.CV />} />
