d610ad7cec86a07cc71b6dea0f0d6d24e2c69afa
Arthur Fincham <arthurfincham@me.com>
2022-01-30 17:42:38 +0000

    cut animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-/* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import * as Pages from './pages/pagesExporter';
-import { animated } from 'react-spring';
 import Navbar from './components/Navbar';
 import useGoogleAnalytics from './utils/useGoogleAnalytics';
 import { useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Animation from './animation/Animation';
-import React, { useState } from 'react';
-import { useSpring } from 'react-spring';
-import { TailSpin } from 'react-loader-spinner';
+import React from 'react';
 
 function GARoutes() {
   useGoogleAnalytics();
@@ -35,66 +30,10 @@ function GARoutes() {
 }
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [entered, setEntered] = useState(true);
-
-  const divStyle = () => {
-    if (loading) {
-      return {
-        opacity: 0,
-      };
-    } else {
-      return {
-        opacity: 1,
-      };
-    }
-  };
-
-  const spinStyle = () => {
-    if (loading) {
-      return {
-        display: 'flex',
-      };
-    } else {
-      return {
-        display: 'none',
-      };
-    }
-  };
-
-  const textStyle = useSpring({
-    delay: 2000,
-    to: 0.45,
-    from: {
-      opacity: 0,
-    },
-  });
-
   return (
-    <>
-      {entered && (
-        <>
-          <animated.div style={spinStyle()} className="w-full h-[100vh] fcc">
-            <TailSpin color="#4F47E6" height={80} width={80} />
-          </animated.div>
-          <animated.div style={divStyle()} className="relative">
-            <animated.button
-              style={textStyle}
-              id="mobileEnterButton"
-              className="fixed z-20 py-3 px-4 rounded-lg bottom-20 left-[50%] enterButton border border-gray-900 text-2xl tracking-wide font-a2 text-gray-900 sm:hidden"
-            >
-              enter
-            </animated.button>
-            <Animation setLoading={setLoading} setEntered={setEntered} textStyle={textStyle} />
-          </animated.div>
-        </>
-      )}
-      {!entered && (
       <Router>
         <GARoutes />
       </Router>
-      )} 
-    </>
   );
 }
 
