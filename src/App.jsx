838ca336e8d0f30dbf93928078f2b156f60b88a7
Arthur Fincham <arthurfincham@me.com>
2022-01-31 11:01:32 +0000

    move GARoutes into its own jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Navbar from './components/Navbar';
-import * as Pages from './pages/pagesExporter';
-import useGoogleAnalytics from './utils/useGoogleAnalytics';
-
-
-function GARoutes() {
-  useGoogleAnalytics();
-
-  const location = useLocation();
-  return (
-    <>
-      <Navbar />
-      <main className="dark:bg-slate-900">
-        <TransitionGroup component={null}>
-          <CSSTransition key={location.key} classNames="fade" timeout={300}>
-            <Routes location={location}>    
-              <Route path="/" element={<Pages.About />} />
-              <Route path="/projects" element={<Pages.Projects />} />
-              <Route path="/CV" element={<Pages.CV />} />
-              <Route path="/contact" element={<Pages.Contact />} />
-            </Routes>
-          </CSSTransition>
-        </TransitionGroup>
-      </main>
-    </>
-  );
-}
+import { BrowserRouter as Router } from 'react-router-dom';
+import GARoutes from './GARoutes'
 
 function App() {
   return (
---
+++ src/GARoutes.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Navbar from './components/Navbar';
+import * as Pages from './pages/pagesExporter';
+import useGoogleAnalytics from './utils/useGoogleAnalytics';
+
+export default function GARoutes() {
+  useGoogleAnalytics();
+
+  const location = useLocation();
+  return (
+    <>
+      <Navbar />
+      <main className="dark:bg-slate-900">
+        <TransitionGroup component={null}>
+          <CSSTransition key={location.key} classNames="fade" timeout={300}>
+            <Routes location={location}>    
+              <Route path="/" element={<Pages.About />} />
+              <Route path="/projects" element={<Pages.Projects />} />
+              <Route path="/CV" element={<Pages.CV />} />
+              <Route path="/contact" element={<Pages.Contact />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </main>
+    </>
+  );
+}
